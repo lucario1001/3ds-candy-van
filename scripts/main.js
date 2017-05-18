725d9934cf03bb54ff1922676cac3f9c5a23da12
rikumax25 <jorgev259@outlook.es>
2017-05-18 00:40:06 -0600

    first prototype

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,21 @@
-var titleDB;
+var titleDB = [];
+var available
 
 
-$.get("https://3ds.titledb.com/v1/url",function(res){titleDB = res; console.log(titleDB)});
+$.get("https://3ds.titledb.com/v1/cia",function(res){enable(res)});
+
+function enable(res){ 
+    for(var i=0;i<res.length;i++){
+        titleDB.push(res[i]["url_id"]);              
+    }
+    
+    $("body").html("<div id='name'></div><div id='qr'></div><button onclick='candy()'>Get some candy</button>");
+}
+
+function candy(){
+    var index = Math.floor(Math.random() * (titleDB.length-1));
+    $.get("https://3ds.titledb.com/v1/url/" + titleDB[index],function(res){
+        $("#qr").html("");
+        new QRCode(document.getElementById("qr"), res.url);
+    });
+}
